4beb21c090716cb3a2d6caec92c2ea638de175d8
Ashwinalvin <79683142+Ashwinalvin@users.noreply.github.com>
2021-09-06 12:28:24 +0530

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 const DEVELOPMENT_URL = "http://localhost:5454";
-const PRODUCTION_URL = "https://calm-forest-64758.herokuapp.com/";
+const PRODUCTION_URL = "https://calm-forest-64758.herokuapp.com";
 
 exports.API_URL = PRODUCTION_URL;
